2a2ed41f52a5cdcb6ee3532ae52a1b22687b1649
marco <makoheuvelman@hotmail.com>
2020-07-06 10:22:42 +0200

    deep sleep done

--- LISA/deep_sleep.py
+++ LISA/deep_sleep.py
@@ -46,7 +46,7 @@ def calculatePadding(L, KSZ, S, D=1):
 
 
 class PCC(nn.Module):
-    def __init__(self, in_channels, out_channels, activation='relu', stride=4, pool=4, kernel=7):
+    def __init__(self, in_channels, out_channels, activation='relu', pool=4, kernel=7):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -55,10 +55,10 @@ class PCC(nn.Module):
 
         self.blocks = nn.Sequential(
             nn.MaxPool1d(pool),
-            nn.Conv1d(in_channels, in_channels, kernel, stride=stride),
+            nn.Conv1d(in_channels, out_channels, kernel, padding=int(np.floor(kernel/2))),
             self.activate,
             nn.BatchNorm1d(out_channels),
-            nn.Conv1d(out_channels, out_channels, kernel, stride=stride),
+            nn.Conv1d(out_channels, out_channels, kernel, padding=int(np.floor(kernel/2))),
             self.activate,
             nn.BatchNorm1d(out_channels)
         )
@@ -69,7 +69,7 @@ class PCC(nn.Module):
 
 
 class UCC(nn.Module):
-    def __init__(self, in_channels, out_channels, activation='relu', stride=4, kernel=7, pool=4):
+    def __init__(self, in_channels, out_channels, activation='relu', kernel=7, pool=4, up_kernel=4):
         super().__init__()
         self.in_channels = in_channels
         self.out_channels = out_channels
@@ -77,25 +77,28 @@ class UCC(nn.Module):
         self.activate = activation_func(activation)
 
         self.up = nn.Sequential(
-            nn.Conv1d(in_channels, out_channels, kernel, stride=pool)
+            nn.ConvTranspose1d(in_channels, out_channels, up_kernel, stride=pool, padding=int(np.floor((up_kernel - pool)//2)))
         )
 
         self.blocks = nn.Sequential(
             # in_channels = concatanated size
-            nn.Conv1d(in_channels, out_channels, kernel, stride=stride),
+            nn.Conv1d(out_channels*2, out_channels, kernel, padding=int(np.floor(kernel/2))),
             self.activate,
             nn.BatchNorm1d(out_channels),
-            nn.Conv1d(out_channels, out_channels, kernel, stride=stride),
+            nn.Conv1d(out_channels, out_channels, kernel, padding=int(np.floor(kernel/2))),
             self.activate,
             nn.BatchNorm1d(out_channels)
         )
 
     def forward(self, x, x2):
 
-        # print("DCU2", x.shape)
+        # print("input", x.shape)
         x = self.up(x)
-        x = torch.cat((x, x2), dim=2)
+        # print(x.shape, x2.shape)
+        x = torch.cat((x, x2), dim=1)
+        # print(x.shape)
         x = self.blocks(x)
+        # print(x.shape)
 
         return x
 
@@ -115,7 +118,7 @@ class Deep_Sleep(nn.Module):
           n_classes: number of classes of the classification problem
 
         """
-        self.INPUT_LENGTH = input_length
+        self.input_length = input_length
         self.INPUT_CHANNELS = channels_to_use
         self.AROUSAL_CLASSES = 3
         self.SLEEP_CLASSES = 6
@@ -139,7 +142,7 @@ class Deep_Sleep(nn.Module):
         self.ucc5 = UCC(30, 25)
         self.ucc6 = UCC(25, 21)
         self.ucc7 = UCC(21, 18)
-        self.ucc8 = UCC(18, self.HIDDEN_SIZE, pool=2)
+        self.ucc8 = UCC(18, self.HIDDEN_SIZE, pool=2, up_kernel=4)
 
         self.arousal_classifier = nn.Sequential(
             nn.Conv1d(self.HIDDEN_SIZE, self.AROUSAL_CLASSES, 1)
@@ -161,27 +164,44 @@ class Deep_Sleep(nn.Module):
         Returns:
           out: outputs of the network
         """
+        # print("input", x.shape)
         pcc1 = self.pcc1(x)
+        # print("pcc1 ", pcc1.shape)
         pcc2 = self.pcc2.forward(pcc1)
+        # print("pcc2 ", pcc2.shape)
         pcc3 = self.pcc3.forward(pcc2)
+        # print("pcc3 ", pcc3.shape)
         pcc4 = self.pcc4.forward(pcc3)
+        # print("pcc4 ", pcc4.shape)
         pcc5 = self.pcc5.forward(pcc4)
+        # print("pcc4 ", pcc5.shape)
         pcc6 = self.pcc6.forward(pcc5)
+        # print("pcc4 ", pcc6.shape)
         pcc7 = self.pcc7.forward(pcc6)
+        # print("pcc4 ", pcc7.shape)
         pcc8 = self.pcc8.forward(pcc7)
+        # print("pcc4 ", pcc8.shape)
         pcc9 = self.pcc9.forward(pcc8)
+        # print("pcc9 ", pcc9.shape)
 
         ucc1 = self.ucc1.forward(pcc9, pcc8)
+        # print("ucc1 ", ucc1.shape)
         ucc2 = self.ucc2.forward(ucc1, pcc7)
+        # print("ucc2 ", ucc2.shape)
         ucc3 = self.ucc3.forward(ucc2, pcc6)
+        # print("ucc3 ", ucc3.shape)
         ucc4 = self.ucc4.forward(ucc3, pcc5)
+        # print("ucc4 ", ucc4.shape)
         ucc5 = self.ucc5.forward(ucc4, pcc4)
+        # print("ucc5 ", ucc5.shape)
         ucc6 = self.ucc6.forward(ucc5, pcc3)
+        # print("ucc6 ", ucc6.shape)
         ucc7 = self.ucc7.forward(ucc6, pcc2)
+        # print("ucc7 ", ucc7.shape)
         ucc8 = self.ucc8.forward(ucc7, pcc1)
-
+        # print("ucc8 ", ucc8.shape)
         out_arousal = self.arousal_classifier(ucc8)
-        out_sleep = self.arousal_classifier(ucc8)
+        out_sleep = self.sleep_classifier(ucc8)
 
-        # print("DONE", out_arousal.shape)
+        # print("DONE", out_arousal.shape, out_sleep.shape)
         return out_arousal, out_sleep
