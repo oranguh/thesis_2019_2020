244ae0a76c89e6eee15f4d1fb392d8e5f93db1f1
marco <makoheuvelman@hotmail.com>
2020-07-10 13:11:18 +0200

    Many changed, i should use git more
    added loading models
    Added AUROC and AUPRC

--- LISA/train.py
+++ LISA/train.py
@@ -19,6 +19,7 @@ from datetime import datetime
 from sacred import Experiment
 from sacred.observers import MongoObserver
 from sacred.observers import FileStorageObserver
+from score2018 import Challenge2018Score
 import platform
 
 
@@ -26,7 +27,7 @@ def initialize_model(model_name, channels_to_use):
     # Initialize these variables which will be set in this if statement. Each of these
     #   variables is model specific.
     model_ft = None
-    input_size = 0
+
     #
 
     if model_name == "ConvNet_IID":
@@ -37,9 +38,6 @@ def initialize_model(model_name, channels_to_use):
         # num_ftrs = model_ft.fc.in_features
         # model_ft.fc = nn.Linear(num_ftrs, num_classes)
 
-        # Window lenght in seconds * Hz of data
-        input_size = 30 * 100
-
     elif model_name == "Howe_Patterson":
         """ Howe_Patterson
         """
@@ -48,15 +46,11 @@ def initialize_model(model_name, channels_to_use):
         # num_ftrs = model_ft.classifier[6].in_features
         # model_ft.classifier[6] = nn.Linear(num_ftrs,num_classes)
 
-        # largest recording length 3422800 at 100Hz
-        input_size = 3422800
 
     elif model_name == "Deep_Sleep":
         """ Deep_Sleep
         """
         # largest recording length 3422800 at 100Hz
-        # For deep sleep we use factors of 2.
-        input_size = 2**21
 
         model_ft = Deep_Sleep(channels_to_use=channels_to_use, input_length=input_size)
         # set_parameter_requires_grad(model_ft, feature_extract)
@@ -67,7 +61,7 @@ def initialize_model(model_name, channels_to_use):
         print("Invalid model name, exiting...")
         exit()
 
-    return model_ft, input_size
+    return model_ft
 
 def load_obj(name):
     with open(name, 'rb') as f:
@@ -77,38 +71,61 @@ def load_obj(name):
 ex = Experiment("hello")
 ex.observers.append(FileStorageObserver('my_runs'))
 ex.observers.__dir__()
-writer = SummaryWriter(comment=str(datetime.now().strftime("%Y%m%d-%H%M%S")))
-
+Challenge2018Scorer = Challenge2018Score()
 
 @ex.config
 def cfg():
 
     # load metadata stuff
     arousal_annotation = ["not_scored", "not_arousal", "Arousal"]
-    weights_arousal = [.0, .1, .9]  # Snooze
-    weights_arousal = [.0, .05, .95]  # SHHS
     sleep_stages = ['nonrem1', 'nonrem2', 'nonrem3', 'rem', 'undefined', 'wake']
-    weights_sleep = [.2, .1, .3, .2, .0, .2]  # for Snooze
-    weights_sleep = [.3, .05, .3, .3, .0, .05]  # for SHHS
 
-    weights_sleep, weights_arousal = [.2, .2, .2, .2, .0, .2], [.0, .5, .5]
+    weights_sleep, weights_arousal = [.2, .2, .2, .2, .0, .2], [.0, .5, .5] # default weights?
+
+    # models to train... Howe_Patterson, U-Net, CNN, cNN + LSTM, wavenet? Model also determines the way the data is loaded.
+
+    # for conv use 2048 batchsize ["ConvNet_IID", "Howe_Patterson"]
+    # model_name = "ConvNet_IID"
+    # for howe use batchsize 6 on lisa?
+    # model_name = "Howe_Patterson"
+    model_name = "Deep_Sleep"
+
+    # Dataset to use.... also determines path. Snooze, SHHS, Philips, HMC
+    data_name = "snooze"
+    data_name = "SHHS"
+    # Predicting future labels is a thing I want to focus on.
+    # pred_future = True
+    pretrained = False
+    pretrained = "models\\Jul09_23-59-04_BananaDeep_Sleep_SHHS"
 
     if platform.system() == 'Windows':
-        data_folder = 'E:\\data\\you-snooze-you-win-the-physionet-computing-in-cardiology-challenge-2018-1.0.0\\marco'
-        data_folder = 'E:\\shhs\\polysomnography\\shh1_numpy'
+        if data_name == "SHHS":
+            data_folder = 'E:\\shhs\\polysomnography\\shh1_numpy'
+            weights_sleep = [.3, .05, .3, .3, .0, .05]  # for SHHS
+            weights_arousal = [.0, .05, .95]  # SHHS
+        else:
+            data_folder = 'F:\\you-snooze-you-win-the-physionet-computing-in-cardiology-challenge-2018-1.0.0\\marco'
+            data_folder = 'D:\\data\\snooze\\marco'
+            weights_sleep = [.2, .1, .3, .2, .0, .2]  # for Snooze
+            weights_arousal = [.0, .1, .9]  # Snooze
+
+
         # Parameters for dataloader
         dataloader_params = {'batch_size': 1,
                              'shuffle': True,
                              'num_workers': 0}
     else:
-        data_folder = '/project/marcoh/you_snooze_you_win/marco/'
-        data_folder = '/project/marcoh/shhs/polysomnography/shh1_numpy/'
-        # Parameters for dataloader
-        dataloader_params = {'batch_size': 8,
-                             'shuffle': True,
-                             'num_workers': 8}
+        if data_name == "SHHS":
+            data_folder = '/project/marcoh/shhs/polysomnography/shh1_numpy/'
+        else:
+            data_folder = '/project/marcoh/you_snooze_you_win/marco/'
 
-    max_epochs = 100
+        # Parameters for dataloader
+        dataloader_params = {'batch_size': 3,
+                             'shuffle': True,
+                             'num_workers': 4}
+
+    max_epochs = 2000
     # full PSG has 12, we use 1
     channels_to_use = 1
 
@@ -116,24 +133,16 @@ def cfg():
                         "optim": "Adam"}
     lr = 1e-4
 
-    # models to train... Howe_Patterson, U-Net, CNN, cNN + LSTM, wavenet? Model also determines the way the data is loaded.
 
-    # for conv use 2048 batchsize ["ConvNet_IID", "Howe_Patterson"]
-    # model_name = "ConvNet_IID"
-    # for howe use batchsize 6 on lisa?
-    model_name = "Howe_Patterson"
-    model_name = "Deep_Sleep"
-
-    # Dataset to use.... also determines path. Snooze, SHHS, Philips, HMC
-    # data_name = "snooze"
-    data_name = "SHHS"
-    # Predicting future labels is a thing I want to focus on.
-    # pred_future = True
 
 @ex.automain
 def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
         data_folder, sleep_stages, arousal_annotation,
-        weights_arousal, weights_sleep, model_name, data_name):
+        weights_arousal, weights_sleep, model_name, data_name, pretrained):
+
+    # writer = SummaryWriter(comment=str(datetime.now().strftime("%Y%m%d-%H%M%S")))
+    writer = SummaryWriter(comment=model_name + "_" + data_name)
+
     # for saving log to file
     # fh = logging.FileHandler('spam.log')
     # _log.addHandler(fh)
@@ -153,7 +162,11 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
     dataloaders = {"train": training_generator, "val": validation_generator}
 
     # models
-    model, inputs = initialize_model(model_name, channels_to_use=channels_to_use)
+    if pretrained:
+        model = torch.load(pretrained)
+        print("Pre-trained model loaded \n", model)
+    else:
+        model = initialize_model(model_name, channels_to_use=channels_to_use)
     model.to(device)
 
     # ignore classes unscored and undefined
@@ -182,6 +195,7 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
             pred_array = np.empty(0)
             true_array_sleep = np.empty(0)
             pred_array_sleep = np.empty(0)
+            Challenge2018Scorer._pos_values, Challenge2018Scorer._neg_values = 0, 0
 
             running_loss = 0.0
             counters = 0
@@ -208,6 +222,7 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
 
                     arousal_out, sleep_out = model.forward(inputs)
                     # print(arousal_out.shape, sleep_out.shape)
+                    # print(annotations_arousal.shape, annotations_sleep.shape)
                     loss_arousal = criterion_arousal(arousal_out, annotations_arousal)
                     loss_sleep = criterion_sleep(sleep_out, annotations_sleep)
                     loss = loss_arousal + loss_sleep
@@ -221,7 +236,13 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
                 true_array_ = annotations_arousal.cpu().numpy().squeeze().astype(int)
                 pred_array_ = arousal_out.argmax(dim=1).cpu().numpy().squeeze().astype(int)
 
+                prediction_prob_arousal = torch.nn.functional.softmax(arousal_out, dim=1)[:, 2,
+                                          :].detach().cpu().numpy().squeeze()
+                Challenge2018Scorer.score_record(true_array_[true_array_ != 0] - 1,
+                                                 prediction_prob_arousal[true_array_ != 0])
 
+                # set all 0 (unscored) to predictions to 1 (not-arousal)
+                pred_array_[pred_array_ == 0] = 1
                 # remove all 0 (unscored) ORDER of pred/True Matters
                 pred_array = np.append(pred_array, pred_array_[true_array_ != 0])
                 true_array = np.append(true_array, true_array_[true_array_ != 0])
@@ -247,6 +268,8 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
             model_savepath = os.path.join("models", model_savepath)
             torch.save(model, model_savepath)
 
+            def save_metrics(arousal_pred, arousal_true, sleep_pred, sleep_true, epoch_loss, writer, epoch):
+                pass
             epoch_loss = running_loss / len(dataloaders[phase].dataset)
             acc = accuracy(pred_array, true_array)
             acc_sleep = accuracy(pred_array_sleep, true_array_sleep)
@@ -259,17 +282,29 @@ def run(_log, max_epochs, channels_to_use, dataloader_params, lr,
             # ex.log_scalar('{}_Accuracy_sleep_staging'.format(phase), acc_sleep, epoch)
 
             # todo
+            AUROC_arousal = Challenge2018Scorer.gross_auroc()
+            writer.add_scalar('AUROC_arousal/{}'.format(phase), AUROC_arousal, global_step=epoch)
+            ex.log_scalar('AUROC_arousal/{}'.format(phase), AUROC_arousal, epoch)
+
+            AUPRC_arousal = Challenge2018Scorer.gross_auprc()
+            writer.add_scalar('AUPRC_arousal_arousal/{}'.format(phase), AUPRC_arousal, global_step=epoch)
+            ex.log_scalar('AUPRC_arousal_arousal/{}'.format(phase), AUPRC_arousal, epoch)
+
+            # print("PRC", Challenge2018Scorer.gross_auprc(), metrics.average_precision_score(true_array, pred_array))
+            # print("ROC", Challenge2018Scorer.gross_auroc(), metrics.roc_auc_score(true_array, pred_array))
+
             balanced_arousal = metrics.balanced_accuracy_score(true_array, pred_array)
             balanced_sleep = metrics.balanced_accuracy_score(true_array_sleep, pred_array_sleep)
-            cohen_kappa_arousal = metrics.cohen_kappa_score(true_array, pred_array)
-            cohen_kappa_sleep = metrics.cohen_kappa_score(true_array_sleep, pred_array_sleep)
-
             writer.add_scalar('Accuracy_arousal/{}'.format(phase), balanced_arousal, global_step=epoch)
             ex.log_scalar('Accuracy_arousal/{}'.format(phase), balanced_arousal, epoch)
             writer.add_scalar('Accuracy_sleep_staging/{}'.format(phase), balanced_sleep, global_step=epoch)
             ex.log_scalar('Accuracy_sleep_staging/{}'.format(phase), balanced_sleep, epoch)
+
+            cohen_kappa_arousal = metrics.cohen_kappa_score(true_array, pred_array)
+            cohen_kappa_sleep = metrics.cohen_kappa_score(true_array_sleep, pred_array_sleep)
             writer.add_scalar('Kappa_arousal/{}'.format(phase), cohen_kappa_arousal, global_step=epoch)
             writer.add_scalar('Kappa_sleep/{}'.format(phase), cohen_kappa_sleep, global_step=epoch)
+
             report = metrics.classification_report(true_array,
                                                    pred_array,
                                                    labels=[0, 1, 2],
