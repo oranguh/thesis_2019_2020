f9c785e1bbffaef6dbe22f6bbf16ac5e3ee49ee8
marco <makoheuvelman@hotmail.com>
2020-10-13 11:37:41 +0200

    sleep staging confusion matrix

--- LISA/visualizations.py
+++ LISA/visualizations.py
@@ -61,6 +61,70 @@ def plot_confusion_matrix(y_true, y_pred, classes,
 
     return fig
 
+
+def sleep_staging_confusion(cm_dict,
+                          title=None,
+                          cmap=plt.cm.Blues):
+    """
+    This function prints and plots the confusion matrix.
+    Normalization can be applied by setting `normalize=True`.
+    """
+
+    sub_titles = ['Normalized confusion matrix', 'Confusion matrix']
+    fig, ax = plt.subplots(7, 2, figsize=(20, 10), sharex="all", sharey="all")
+    fig.suptitle('{}'.format(title), fontsize=16)
+    # Compute confusion matrix
+    # Only use the labels that appear in the data
+    # classes = classes[unique_labels(y_true, y_pred)]
+    cmap = 'RdYlGn'
+    cm_total = np.asarray(list(cm_dict.values())).sum(axis=0)
+    for j, stage in enumerate(["N1", "N2", "N3", "REM", "Undefined", "Wake", "Sum"]):
+        if stage == "Sum":
+            cm = cm_total
+        # elif stage == "Undefined":
+        #     j -= 1
+        #     continue
+        else:
+            cm = cm_dict[stage]
+        for i, normalization_flag in enumerate([True, False]):
+
+            if normalization_flag:
+                cm_plot = cm.astype('float') / cm.sum(axis=1)[:, np.newaxis]
+                fmt = '.1%'
+            else:
+                cm_plot = cm
+                fmt = 'd'
+
+            # cmap = sns.diverging_palette(220, 20, n=7)
+
+            # sns.heatmap(cm, annot=True, ax=ax, center=1/len(classes), cmap=cmap, robust=True, cbar=True)  # annot=True to annotate cells
+            sns.heatmap(cm_plot, annot=True, ax=ax[j, i], cmap=cmap, robust=True, cbar=False, fmt=fmt)
+            # labels, title and ticks
+            # ax[j, i].set_xlabel('Predicted labels')
+            # ax[j, i].set_ylabel('True labels')
+            # print(cm.sum(axis=1)/cm_total.sum(axis=1))
+            blup = "{stage}: {sleep_total:.0f}% of sleep; arousal: {conf[1]:.0f}%".format(stage=stage,
+                                                                                   sleep_total= cm.sum()/cm_total.sum()*100,
+                                                                                   conf=cm.sum(axis=1)/cm_total.sum(axis=1)*100)
+            ax[j, i].set_title(blup)
+            ax[j, i].xaxis.set_ticklabels(["Pred not arousal", "Pred arousal"])
+            ax[j, i].yaxis.set_ticklabels(["True not arousal", "True arousal"], rotation=45, rotation_mode="anchor")
+
+            # ax.set_ylim(len(classes) + 0.5, -0.5)
+
+
+    # plt.tight_layout()
+
+    Save_folder = Path("figures/confusions") / title
+    Save_folder.mkdir(parents=True, exist_ok=True)
+
+    filename = title + '.png'
+    plt.savefig(Save_folder / filename)
+    # plt.show()
+
+    return fig
+
+
 def plot_classes_distribution(labels, classes):
 
     # plt.bar(list(labels.keys()), labels.values(), 1, color='g')
